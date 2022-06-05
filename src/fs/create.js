fc872a2a6f45a832c71006157212d56e63dbb642
Horbach-Ivan <105849038+Horbach-Ivan@users.noreply.github.com>
2022-06-05 18:19:42 +0300

    Update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,19 @@
+import fs from 'fs'
 export const create = async () => {
-    // Write your code here 
+    const freshFile = "fresh.txt"
+
+    fs.readdir("./files", (err, files) => {
+        const checkContains = files.includes(freshFile)
+
+        if (checkContains) {
+            throw new Error(' FS operation failed')
+        } else {
+        fs.writeFile("./files/fresh.txt", "I am fresh and young", (err)  => {
+                if(err) {
+                    return console.log(err);
+                }
+            }); 
+        }
+      });
+    
 };
